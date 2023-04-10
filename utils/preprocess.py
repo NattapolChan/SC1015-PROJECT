78f7a1aa8be6b5322a0eb6f52d2fb0386cd3d45f
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2023-04-10 22:50:08 +0800

    Add pytorch dataloader class

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,45 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import torch
 import torch.nn as nn
+from torch.utils.data import Dataset
+import matplotlib
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
 
+
+PROJECT_DIR = 'path/to/SC1015_PROJECT'
+class set_b_dataset(Dataset):
+    def __init__(self, path: str):
+        super().__init__()
+        self.path = path
+        self.metadata = pd.read_csv(f'{PROJECT_DIR}dataset/set_b.csv')
+        self.filenames = []
+        for name in self.metadata['fname']: 
+            if 'Bunlabelled' not in name: 
+                self.filenames.append(name)
+        self.dataset = []
+        self.fft = []
+        for fn in self.filenames:
+            data, samplerate = librosa.load(f'{PROJECT_DIR}dataset/{fn}')
+            self.dataset.append([data, samplerate])
+            self.fft.append(librosa.amplitude_to_db(librosa.stft(data)))
+        
+    def __len__(self) -> int:
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx: int) -> tuple[np.ndarray, int]:
+        return self.dataset[idx][0], self.dataset[idx][1]
+        
+    def show_wave(self, idx: int, **kwargs) -> librosa.display.AdaptiveWaveplot:
+        return librosa.display.waveshow(self.dataset[idx][0], sr=self.dataset[idx][1], *kwargs)
+    
+    def show_spec(self, idx: int, **kwargs) -> matplotlib.collections.QuadMesh:
+        return librosa.display.specshow(self.fft[idx], *kwargs)
+    
+    def denoise(self) -> None:
+        # Add denoise function
+        # denoise self.dataset
+        # dtype(self.dataset): [magnitude array: float, samplerate: int]
+        pass
