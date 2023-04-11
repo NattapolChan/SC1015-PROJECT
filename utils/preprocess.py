577ecb7f6fa92089bc50b09a9141947a944d1290
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2023-04-12 02:03:46 +0800

    add more EDA

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -7,6 +7,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
+import scipy
 
 
 PROJECT_DIR = '/Users/nattapolchanpaisit/Documents/GitHub/Algorithm/SC1015-PROJECT/'
@@ -26,8 +27,24 @@ class set_b_dataset(Dataset):
             self.dataset.append([data, samplerate])
             self.fft.append(librosa.amplitude_to_db(librosa.stft(data)))
         
+        bpm = []
+        peak_interval = []
+        peak_interval_timer = []
+        for i in range(len(self.dataset)):
+            peak_array = self.aproximate_bpm(i)
+            interval = [(peak_array[i+1] - peak_array[i])/self.dataset[i][1] for i in range(len(peak_array)-1)]
+            timer = [peak_array[i]/self.dataset[i][1] for i in range(len(peak_array)-1)]
+            val = 60 * len(peak_array) * self.dataset[i][1] / len(self.dataset[i][0]) / 2
+            bpm.append(val)
+            peak_interval.append(interval)
+            peak_interval_timer.append(timer)
+        
+        self.metadata['bpm'] = bpm
+        self.metadata['peak-interval'] = peak_interval
+        self.metadata['peak-interval-timer'] = peak_interval_timer
+        
     def __len__(self) -> int:
-        return len(self.landmarks_frame)
+        return len(self.dataset)
 
     def __getitem__(self, idx: int) -> tuple[np.ndarray, int]:
         return self.dataset[idx][0], self.dataset[idx][1]
@@ -43,3 +60,8 @@ class set_b_dataset(Dataset):
         # denoise self.dataset
         # dtype(self.dataset): [magnitude array: float, samplerate: int]
         pass
+    
+    def aproximate_bpm(self, index):
+        peak_array, _ = scipy.signal.find_peaks(self.dataset[index][0], height=0.1, 
+                                                distance=0.2*self.dataset[index][1])
+        return peak_array
